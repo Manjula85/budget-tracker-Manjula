51f93df184849ecd301bb242665cfe25e4a27092
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2021-04-18 19:02:52 -0400

    service worker added

--- public/service-worker.js
+++ public/service-worker.js
@@ -30,22 +30,37 @@ self.addEventListener('install', function (e) {
   });
 
 // Activation step
-self.addEventListener('activate', function (e){
-    e.waitUntil(
-        caches.keys().then(function (keylist){
-            let cacheKeeplist = keyList.filter(function (key){
-                return key.indexOf(APP_PREFIX);
-            });
-            cacheKeeplist.push(CACHE_NAME);
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      let cacheKeeplist = keyList.filter(function(key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      cacheKeeplist.push(CACHE_NAME);
 
-            return Promise.all(
-                keyList.map(function(key,i){
-                    if(cacheKeeplist.indexOf(key)===-1{
-                        console.log('deleting cache : ' + keyList[i]);
-                        return caches.delete(keyList[i]);
-                    })
-                })
-            )
+      return Promise.all(
+        keyList.map(function(key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log('deleting cache : ' + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
         })
-    )
-})
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function (e){
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request){
+      if (request){ //if cache is available
+        console.log('respond with cache : ', + e.request.url)
+        return request
+      } else {  //if there is no cache
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
+    })
+  );
+});
